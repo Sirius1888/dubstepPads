3ac2d0c6973a02ab3a25d329409385904cd9e4ae
Karukes Sergey <ser1888kg@gmail.com>
2019-01-30 12:58:25 +0600

    added: change background buttons

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.myapplication
 
+import android.animation.ValueAnimator
 import android.content.Context
 import android.graphics.Color
+import android.graphics.Color.WHITE
 import android.media.AudioManager
 import android.media.AudioManager.STREAM_MUSIC
 import android.media.SoundPool
@@ -9,6 +11,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.view.View
+import android.view.animation.Animation
+import android.widget.Button
 import androidx.core.os.postDelayed
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.IOException
@@ -50,83 +54,115 @@ class MainActivity : AppCompatActivity() {
         tinkPlaySound(soundIdTink)
 
     }
+    private fun resetColor(button: Button) {
+        Handler().postDelayed({
+            button.setBackgroundColor(getResources().getColor(R.color.whiteButtons))
+        }, 1010)
+    }
+
+    private fun resetColor() {
+        Handler().postDelayed({
+            boom_play_sound_button.setBackgroundColor(getResources().getColor(R.color.whiteButtons))
+            clap_play_sound_button.setBackgroundColor(getResources().getColor(R.color.whiteButtons))
+            hihat_play_sound_button.setBackgroundColor(getResources().getColor(R.color.whiteButtons))
+            kick_play_sound_button.setBackgroundColor(getResources().getColor(R.color.whiteButtons))
+            openhat_play_sound_button.setBackgroundColor(getResources().getColor(R.color.whiteButtons))
+            ride_play_sound_button.setBackgroundColor(getResources().getColor(R.color.whiteButtons))
+            snare_play_sound_button.setBackgroundColor(getResources().getColor(R.color.whiteButtons))
+            tink_play_sound_button.setBackgroundColor(getResources().getColor(R.color.whiteButtons))
+        }, 1010)
+    }
+
+
+    fun changColorAnim(button: Button) {
+        val anim: ValueAnimator = ValueAnimator.ofFloat(0f, 1f)
+        anim.setDuration(1000)
+        val hsv: FloatArray = floatArrayOf(0f,1f,1f)
+        var runColor: Int
+
+        anim.addUpdateListener(ValueAnimator.AnimatorUpdateListener() { animation: ValueAnimator ->
+            hsv[0] = 360 * animation.animatedFraction // 360 * animation.animatedFraction
+            runColor = Color.HSVToColor(hsv)
+            button.setBackgroundColor(runColor)
+
+        })
+        anim.repeatCount = Animation.ABSOLUTE
+        anim.start()
+    }
+
 
     fun boomPlaySound(soundIdBoom: Int) {
         boom_play_sound_button.setOnClickListener {
             soundPool.play(soundIdBoom, 1F, 1F,0,0, 1F)
-            mRunnable = Runnable {
-                boom_play_sound_button.setBackgroundColor(randomHSVColor())
-                mHandler.postDelayed(mRunnable, 100)
-            }
-
-//            val timer = Timer()
-//            val monitor = object : TimerTask() {
-//                override fun run() {
-//                boom_play_sound_button.setBackgroundColor(resources.getColor(R.color.myColor))
-//                }
-//            }
-//            timer.schedule(monitor, 100, 100)
-
-
+            changColorAnim(boom_play_sound_button)
+//            resetColor(boom_play_sound_button)
+            resetColor()
         }
+
     }
 
-    fun randomHSVColor(): Int {
-        // Generate a random hue value between 0 to 360
-        val hue = mRandom.nextInt(361)
-        // We make the color depth full
-        val saturation = 1.0f
-        // We make a full bright color
-        val value = 1.0f
-        // We avoid color transparency
-        val alpha = 255
-        // Finally, generate the color
-        // Return the color
-        return Color.HSVToColor(alpha, floatArrayOf(hue.toFloat(), saturation, value))
-    }
 
 
     private fun clapPlaySound(soundIdClap: Int) {
         clap_play_sound_button.setOnClickListener {
             soundPool.play(soundIdClap, 1F, 1F,0,0, 1F)
-            boom_play_sound_button.setTextColor(randomHSVColor())
-
+            changColorAnim(clap_play_sound_button)
+//            resetColor(clap_play_sound_button)
+            resetColor()
         }
     }
 
     private fun hihatPlaySound(soundIdHihat: Int) {
         hihat_play_sound_button.setOnClickListener {
             soundPool.play(soundIdHihat, 1F, 1F,0,0, 1F)
+            changColorAnim(hihat_play_sound_button)
+//            resetColor(hihat_play_sound_button)
+            resetColor()
         }
     }
 
     private fun kickPlaySound(soundIdKick: Int) {
         kick_play_sound_button.setOnClickListener {
             soundPool.play(soundIdKick, 1F, 1F,0,0, 1F)
+            changColorAnim(kick_play_sound_button)
+//            resetColor(kick_play_sound_button)
+            resetColor()
         }
     }
 
     private fun openhatPlaySound(soundIdOpenHat: Int) {
         openhat_play_sound_button.setOnClickListener {
             soundPool.play(soundIdOpenHat, 1F, 1F,0,0, 1F)
+            changColorAnim(openhat_play_sound_button)
+//            resetColor(openhat_play_sound_button)
+            resetColor()
         }
     }
 
     private fun ridePlaySound(soundIdRide: Int) {
         ride_play_sound_button.setOnClickListener {
             soundPool.play(soundIdRide, 1F, 1F,0,0, 1F)
+            changColorAnim(ride_play_sound_button)
+//            resetColor(ride_play_sound_button)
+            resetColor()
         }
     }
 
     private fun snarePlaySound(soundIdSnare: Int) {
         snare_play_sound_button.setOnClickListener {
             soundPool.play(soundIdSnare, 1F, 1F,0,0, 1F)
+            changColorAnim(snare_play_sound_button)
+//            resetColor(snare_play_sound_button)
+            resetColor()
         }
     }
 
     private fun tinkPlaySound(soundIdTink: Int) {
         tink_play_sound_button.setOnClickListener {
             soundPool.play(soundIdTink, 1F, 1F,0,0, 1F)
+            changColorAnim(tink_play_sound_button)
+//            resetColor(tink_play_sound_button)
+            resetColor()
         }
     }
 }
